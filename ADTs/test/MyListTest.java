a5a64229a337d48c96c60543cbf62cbfa3ac01ed
ElNelsonn <santbord@gmail.com>
2024-05-15 17:50:12 -0300

    ADT update

--- ADTs/test/MyListTest.java
+++ ADTs/test/MyListTest.java
@@ -1,17 +1,30 @@
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import uy.edu.um.prog2.adt.List.MyList;
 import uy.edu.um.prog2.adt.List.MyListImpl;
 import uy.edu.um.prog2.adt.exceptions.EmptyListException;
 import uy.edu.um.prog2.adt.exceptions.OutOfRangeException;
 
-public class MyListTest {
+public class MyListTest<T> {
+    private MyList<String> listTest;
+    private String elementTest1 = "First Element";
+    private String elementTest2 = "Second Element";
+    private String elementTest3 = "Third Element";
+
+    @BeforeClass
+    public void setUp() {
+        listTest = new MyListImpl<>();
+        elementTest1 = "First Element";
+        elementTest2 = "Second Element";
+        elementTest3 = "Third Element";
+    }
+
+
 
     @Test
     public void testAddFunction() {
-        MyListImpl<String> listTest = new MyListImpl<>();
-        String elementTest1 = "First Element";
-        String elementTest2 = "Second Element";
-        String elementTest3 = "Third Element";
         listTest.add(elementTest1);
         listTest.add(elementTest2);
         listTest.add(elementTest1);
@@ -25,10 +38,6 @@ public class MyListTest {
     @Test
     public void testGetFunction() {
         try {
-            MyListImpl<String> listTest = new MyListImpl<>();
-            String elementTest1 = "First Element";
-            String elementTest2 = "Second Element";
-            String elementTest3 = "Third Element";
             listTest.add(elementTest1);
             listTest.add(elementTest2);
             listTest.add(elementTest3);
